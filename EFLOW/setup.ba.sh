c9c73da7c2071a6e4b197bef7cbeaaf7162b51ab
Juan Guerrero <JCystems@gMail.com>
2023-02-06 16:29:07 -0600

    scripts

--- EFLOW/setup.ba.sh
+++ EFLOW/setup.ba.sh
@@ -1,4 +1,5 @@
-# SRC https://learn.microsoft.com/en-us/azure/iot-edge/how-to-create-transparent-gateway?view=iotedge-1.4&tabs=eflow
+# SRC: https://learn.microsoft.com/en-us/azure/iot-edge/how-to-create-transparent-gateway?view=iotedge-1.4&tabs=eflow
+# SRC: https://learn.microsoft.com/en-us/azure/iot-edge/how-to-connect-downstream-iot-edge-device?view=iotedge-1.4&tabs=azure-cli
 
 # WARNING
 # ~ is /home/iotedge-user
@@ -6,12 +7,21 @@
 # it would be /root
 cp --verbose -r ./* /
 
-chown aziotcs:aziotcs /var/aziot/certs
+# Give aziotcs ownership to certificates
+# Read and write for aziotcs, read-only for others
+chown -R aziotcs:aziotcs /var/aziot/certs
 chown aziotcs:aziotcs /var/aziot/certs/*.cert.pem
 chmod 755 /var/aziot/certs
+find /var/aziot/certs -type f -name "*.*" -exec chmod 644 {} \;
 
-chown aziotks:aziotks /var/aziot/secrets
+# Give aziotks ownership to private keys
+# Read and write for aziotks, no permission for others
+chown -R aziotks:aziotks /var/aziot/secrets
 chmod 700 /var/aziot/secrets
 chmod 600 /var/aziot/secrets/*.key.pem
+find /var/aziot/secrets -type f -name "*.*" -exec chmod 600 {} \;
+
+# Verify permissions of directories and files
+ls -Rla /var/aziot
 
 iotedge config apply
---
+++ scripts/certs/CA/certGen_nested_test.ba.sh
@@ -0,0 +1,14 @@
+# SRC: https://learn.microsoft.com/en-us/azure/iot-edge/how-to-connect-downstream-iot-edge-device?view=iotedge-1.4&tabs=azure-cli
+
+# !!! For test only - do not use in production !!!
+
+# Create the the root CA test certificate
+./certGen.sh create_root_and_intermediate
+
+# Create the parent (gateway) device test certificate
+# signed by the shared root CA certificate
+./certGen.sh create_edge_device_ca_certificate "TEST-gateway"
+
+# Create the downstream device test certificate
+# signed by the shared root CA certificate
+./certGen.sh create_edge_device_ca_certificate "TEST-connectivity"
