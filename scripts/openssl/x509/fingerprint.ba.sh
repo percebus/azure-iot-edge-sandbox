f366d693f8464d86a12dbcf623717891102e9778
Juan Guerrero <JCystems@gMail.com>
2023-02-07 11:19:11 -0600

    refactor(fingerprint.ba.sh)

--- scripts/openssl/x509/fingerprint.ba.sh
+++ scripts/openssl/x509/fingerprint.ba.sh
@@ -7,6 +7,7 @@ openssl x509 -in ${ca_cert_file} -text -fingerprint > ${x509_file}
 fingerprint_file="${ca_cert_name}.cert.pem.fingerprint.x509.txt"
 
 # Extract "Fingerprint" from file
+# FIXME use an -out flag of sorts, instead of >
 fingerprint=$(grep -F "Fingerprint" ${x509_file} | cut -d ' ' -f 2 | cut -d '=' -f 2)
 echo "Fingerprint=${fingerprint}"
-echo ${fingerprint} > ${fingerprint_file}
+echo ${fingerprint} | sed 's/[:]//g' > ${fingerprint_file}
